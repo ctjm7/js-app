3f5813043e5bb74d7a950e86fa60fa7cd29dc2ce
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-07-03 14:11:20 -0700

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'coffee';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
+  { name: 'Charmander', heigtht: 0.6, types: ['fire'] },
+  { name: 'Squirtle', height: 0.5, types: ['water'] },
+  { name: 'Jigglypuff', height: 0.5, types: ['normal', 'fairy'] }
+];
