53ab915fab783f922703e8d0e56ee9adaf8d2401
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-07-04 17:31:27 -0700

    created condition to return name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 let pokemonList = [
   { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
-  { name: 'Charmander', heigtht: 0.6, types: ['fire'] },
+  { name: 'Charmander', height: 0.6, types: ['fire'] },
   { name: 'Squirtle', height: 0.5, types: ['water'] },
   { name: 'Jigglypuff', height: 0.5, types: ['normal', 'fairy'] }
 ];
+
+// returns pokemon name and height. If height is greater than 1.0 also returns "Wow, that's big!" //
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 1.0) {
+     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+   } else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!` + '<br>');
+   }
+}
