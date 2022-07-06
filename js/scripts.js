5e34be964f03f45e5f63d6db352e46bdd3da6e73
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-07-06 14:22:37 -0700

    created iife to return pokemon list and add pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-let pokemonList = [
-  { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
-  { name: 'Charmander', height: 0.6, types: ['fire'] },
-  { name: 'Squirtle', height: 0.5, types: ['water'] },
-  { name: 'Jigglypuff', height: 0.5, types: ['normal', 'fairy'] }
-];
-
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, types: ['fire'] },
+    { name: 'Squirtle', height: 0.5, types: ['water'] },
+    { name: 'Jigglypuff', height: 0.5, types: ['normal', 'fairy'] }
+  ];
+// adds new pokemon to list
+  function add (pokemon){
+    pokemonList.push(pokemon);
+  }
+  // returns list of pokemon and characteristics
+  function getAll () {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 // returns pokemon name and height. If height is greater than 1.0 also returns "Wow, that's big!" //
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height < 1.0) {
     document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>');
   } else {
