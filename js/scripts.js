6621165f50866bca367294d44d504afed99e49f7
ctjm7 <107513125+ctjm7@users.noreply.github.com>
2022-07-07 16:26:28 -0700

    created buttons for list and add function criteria

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,53 @@ let pokemonRepository = (function () {
     { name: 'Squirtle', height: 0.5, types: ['water'] },
     { name: 'Jigglypuff', height: 0.5, types: ['normal', 'fairy'] }
   ];
-// adds new pokemon to list
-  function add (pokemon){
-    pokemonList.push(pokemon);
-  }
-  // returns list of pokemon and characteristics
+
+  //adds new pokemon and characteristics to list and checks for object
+  function add (pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'types' in pokemon
+      ) {
+        pokemonList.push(pokemon);
+      } else {
+        console.log('Pokemon input is not correct!');
+      }
+    }
+
+    // returns list of pokemon and characteristics
   function getAll () {
     return pokemonList;
   }
+
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    // pokemon is assigned to an unordered list
+    let listPokemon = document.createElement('li');
+    // assigns a button for each pokemon
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-pokemon-list');
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    // returns pokemon in console when specific pokemon is clicked on in browser
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+  }
+  // retrieves pokemon information to be output in console
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
   return {
-    add: add,
-    getAll: getAll
+    add,
+    getAll,
+    addListItem
   };
 })();
-// returns pokemon name and height. If height is greater than 1.0 also returns "Wow, that's big!" //
+
+// returns pokemon names in a list as a button
 pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height < 1.0) {
-    document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>');
-  } else {
-   document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!` + '<br>');
-  }
+  pokemonRepository.addListItem(pokemon);
 });
